01a08c93865d2a17e525c95fdcc7931bc15d791e
gerar <gerardangeloprado@gmail.com>
2023-01-08 02:04:10 +0800

    First Commit !!!!

---
+++ app/src/main/java/com/codeseasy/examples/sticapass/HealthForm.java
@@ -0,0 +1,14 @@
+package com.codeseasy.examples.sticapass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healthform);
+    }
+}
---
+++ app/src/main/java/com/codeseasy/examples/sticapass/MainActivity.java
@@ -0,0 +1,70 @@
+package com.codeseasy.examples.sticapass;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_scan, btn_logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        btn_logout = findViewById(R.id.btn_logout);
+        btn_scan =findViewById(R.id.btnScanner);
+        btn_scan.setOnClickListener(v -> {
+            scanCode();
+        });
+//        btn_logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), Login.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+        Button btnScanner= findViewById(R.id.btnScanner);
+        btnScanner.setCompoundDrawablesWithIntrinsicBounds(R.drawable.scan , 0, 0, 0);
+
+
+    }
+
+
+
+    private void scanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
+    {
+        if (result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+}
